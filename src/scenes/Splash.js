48ce20f3d486716d32100f78da55b98fba2e2d2c
Pablo Benmaman <montenegro.pablo@outlook.com>
2019-12-14 17:10:56 +0100

    bump versions

--- src/scenes/Splash.js
+++ src/scenes/Splash.js
@@ -12,11 +12,6 @@ export default class extends Phaser.Scene {
      *
      */
         this.load.image('blocks', 'assets/images/blocks.png');
-        this.load.image('broken_blocks', 'assets/images/broken_blocks.png');
-        this.load.image('items', 'assets/images/items.png');
-        this.load.image('base', 'assets/images/base.png');
-        this.load.image('ball', 'assets/images/ball.png');
-        this.load.tilemapTiledJSON('blocksTilemap', 'src/tilesets/blocks.json');
     }
 
     create() {
