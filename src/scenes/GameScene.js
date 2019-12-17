0709ca6afe159110046788023a38a4cb29dec797
Pablo Benmaman <montenegro.pablo@outlook.com>
2019-12-18 00:34:26 +0100

    holy shit i need to go to bed

--- src/scenes/GameScene.js
+++ src/scenes/GameScene.js
@@ -1,8 +1,6 @@
 /* globals __DEV__ */
 import Phaser from 'phaser';
 
-import Mushroom from '../sprites/Mushroom';
-
 export default class extends Phaser.Scene {
     constructor() {
         super({ key: 'GameScene' });
@@ -11,6 +9,18 @@ export default class extends Phaser.Scene {
     init() {
         // TODO
         this.hero = {};
+        this.speed = 100;
+        this.jumpSpeed = 400;
+        this.spacebarKey = this.input.keyboard.addKey('SPACE');
+
+        this.config = {
+            tileSize: 16,
+        };
+
+        this.config = {
+            ...this.config,
+            heroXPosition: this.game.config.width - this.config.tileSize,
+        };
     }
 
     preload() {
@@ -28,17 +38,26 @@ export default class extends Phaser.Scene {
          * this.add.existing(this.mushroom);
          */
 
+        console.log(this.config);
         this.add.image(0, 0, 'background').setY(100);
 
+        this.hero = this.physics.add.sprite(this.config.heroXPosition, 600, 'hero', 'drop_10')
+            .setOrigin(0, 0)
+            .setImmovable();
+
+        // this.hero.body.height = 15;
+        this.hero.body.width = 20;
+        console.log(this.hero.body);
+
         this.blocksGroup = this.physics.add.group();
-        const height = 320;
+        const height = this.config.tileSize * 20;
         let y = -height;
         for (let i = 0; i < 4; i++) {
             const leftBlock = this.make.tileSprite({
                 x: 0,
                 y,
                 height,
-                width: 16,
+                width: this.config.tileSize,
                 // angle: 90,
                 key: 'tiles',
                 frame: 29,
@@ -48,10 +67,10 @@ export default class extends Phaser.Scene {
                 },
             });
             const rightBlock = this.make.tileSprite({
-                x: 134,
+                x: this.config.heroXPosition,
                 y,
                 height,
-                width: 16,
+                width: this.config.tileSize,
                 // angle: 90,
                 key: 'tiles',
                 frame: 29,
@@ -63,15 +82,15 @@ export default class extends Phaser.Scene {
             });
             this.blocksGroup.add(leftBlock);
             this.blocksGroup.add(rightBlock);
-            y += height;
+            leftBlock.body.setImmovable();
+            rightBlock.body.setImmovable();
+            y += height - this.config.tileSize;
         }
         console.log(this.blocksGroup);
-        this.blocksGroup.setVelocity(0, 100);
+        console.log(this.hero);
+        this.blocksGroup.setVelocity(0, this.speed);
 
-        this.hero = this.add.sprite(134, 600, 'hero', 'drop_01')
-            .setOrigin(0, 0)
-            .setAngle(90);
-
+        // creates animation
         this.anims.create({
             key: 'walking',
             frames: this.anims.generateFrameNames('hero', {
@@ -82,15 +101,48 @@ export default class extends Phaser.Scene {
             repeat: -1,
         });
 
+        this.anims.create({
+            key: 'jumping',
+            frames: this.anims.generateFrameNames('hero', {
+                frames: ['drop_06', 'drop_07', 'drop_08'],
+            }),
+            frameRate: 6,
+        });
+
         this.hero.anims.play('walking');
+        this.physics.add.collider(this.hero, this.blocksGroup);
     }
 
     update() {
-        // console.log(this.blocksGroup.children.entries);
+        if (Phaser.Input.Keyboard.JustDown(this.spacebarKey)) {
+            this.hero.anims.play('jumping');
+
+            if (!this.hero.flipX) {
+                this.hero.setVelocityX(-this.jumpSpeed);
+            } else {
+                this.hero.setVelocityX(this.jumpSpeed);
+            }
+
+            // this.hero.flipX = !this.hero.flipX;
+        }
+
+        if (this.hero.x < this.config.tileSize) {
+            this.hero.setVelocityX(0);
+            this.hero.x = this.config.tileSize;
+            this.hero.flipX = true;
+            this.hero.anims.play('walking');
+        }
+
+        if (this.hero.x > this.config.heroXPosition) {
+            this.hero.setVelocityX(0);
+            this.hero.x = this.config.heroXPosition;
+            this.hero.flipX = false;
+            this.hero.anims.play('walking');
+        }
 
         for (const sprite of this.blocksGroup.children.entries) {
-            if (sprite.y - 320 > 640) {
-                sprite.y = -320;
+            if (sprite.y - 320 >= 640) {
+                sprite.y = -(this.config.tileSize * 10);
             }
         }
     }
