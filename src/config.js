d51ee5e445a380e9776f6e2ad4a824a2aecec933
Pablo Benmaman <montenegro.pablo@outlook.com>
2019-12-21 01:17:01 +0100

    gotinha

--- src/config.js
+++ src/config.js
@@ -3,18 +3,25 @@ import Phaser from 'phaser';
 export default {
     type: Phaser.AUTO,
     parent: 'content',
-    width: 500,
-    height: 300,
-    localStorageName: 'phaseres6webpack',
+    width: 150,
+    height: 270,
+    scale: {
+        mode: Phaser.Scale.WIDTH_CONTROLS_HEIGHT,
+        autoCenter: Phaser.Scale.CENTER_BOTH,
+    },
+    autoRound: false,
+    localStorageName: 'phasergamegotinha',
     pixelArt: true,
     physics: {
         default: 'arcade',
         arcade: {
             enableBody: true,
-            debug: false,
-            gravity: {
-                y: 500,
-            },
+            /*
+             * debug: true,
+             * gravity: {
+             *     y: 500,
+             * },
+             */
         },
     },
 };
--- src/utils.js
+++ src/utils.js
@@ -3,3 +3,14 @@ export const centerGameObjects = (objects) => {
         object.anchor.setTo(0.5);
     });
 };
+
+export const generateRandomPositionsArray = (quantity) => {
+    const array = [0, 1, 0];
+    for (let i = 0; i < quantity; i++) {
+        array.push(0, Math.round(Math.random() + 0.2), 0);
+    }
+
+    array.push(0, 1);
+
+    return array;
+};
